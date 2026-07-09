afb26e955134ebe2f9ff0915633bfd0f48dc2de6
刘大利 <1120374193@qq.com>
2026-07-09 11:52:05 +0800

    Delete php directory

--- php/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * 桃之小程序 - 自动跳转到后台管理
- */
-$proto = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https' : 'http';
-$host = $_SERVER['HTTP_HOST'] ?? 'taozhi.433345.xyz';
-header('Location: ' . $proto . '://' . $host . '/admin/login.php');
-exit;
